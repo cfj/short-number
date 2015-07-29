8045b9f85260a218992d25777517906e39d96a6d
Hyubs Ursua <hyubs.u@gmail.com>
2015-07-29 18:02:40 +0800

    Bower compatibility

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
-'use strict';
-module.exports = function (num) {
-    if(typeof num !== 'number') {
-        throw new TypeError('Expected a number');
-    }
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
+    typeof define === 'function' && define.amd ? define(factory) :
+    global.shortNumber = factory();
+}(this, function (num) {
+    'use strict';
 
-    var shortNumber;
-    var exponent;
-    var suffixes = ['K', 'M', 'B', 'T'];
-    var size = (num + '').length;
+    return function (num) {
+        if(typeof num !== 'number') {
+            throw new TypeError('Expected a number');
+        }
 
-    exponent = size % 3 === 0 ? size - 3 : size - (size % 3);
+        var shortNumber;
+        var exponent;
+        var suffixes = ['K', 'M', 'B', 'T'];
+        var size = (num + '').length;
 
-    if(num < 1000) {
-        return num;
-    } else {
-        shortNumber = Math.round(10 * (num / Math.pow(10, exponent))) / 10;
-    }
+        exponent = size % 3 === 0 ? size - 3 : size - (size % 3);
 
-    if(exponent < 6) {
-        shortNumber += suffixes[0];
-    } else if(exponent < 9) {
-        shortNumber += suffixes[1];
-    } else if(exponent < 12) {
-        shortNumber += suffixes[2];
-    } else if(exponent < 16) {
-        shortNumber += suffixes[3];
-    }
+        if(num < 1000) {
+            return num;
+        } else {
+            shortNumber = Math.round(10 * (num / Math.pow(10, exponent))) / 10;
+        }
 
-    return shortNumber;
-};
+        if(exponent < 6) {
+            shortNumber += suffixes[0];
+        } else if(exponent < 9) {
+            shortNumber += suffixes[1];
+        } else if(exponent < 12) {
+            shortNumber += suffixes[2];
+        } else if(exponent < 16) {
+            shortNumber += suffixes[3];
+        }
+
+        return shortNumber;
+    };
+}));
