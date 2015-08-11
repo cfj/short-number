9f759aa8d70f53f4280f91a0e400b32b7c15484c
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-08-11 16:13:43 +0200

    better way of adding the suffix

--- index.js
+++ index.js
@@ -20,20 +20,22 @@
 
         var shortNumber;
         var exponent;
-        var suffixes = ['K', 'M', 'B', 'T'];
+        var suffixes = {
+            'K': 6,
+            'M': 9,
+            'B': 12,
+            'T': 16
+        };
         var size = Math.floor(Math.log(num) / Math.log(10)) + 1;
 
         exponent = size % 3 === 0 ? size - 3 : size - (size % 3);
         shortNumber = Math.round(10 * (num / Math.pow(10, exponent))) / 10;
 
-        if(exponent < 6) {
-            shortNumber += suffixes[0];
-        } else if(exponent < 9) {
-            shortNumber += suffixes[1];
-        } else if(exponent < 12) {
-            shortNumber += suffixes[2];
-        } else if(exponent < 16) {
-            shortNumber += suffixes[3];
+        for(var suffix in suffixes) {
+            if(exponent < suffixes[suffix]) {
+                shortNumber += suffix;
+                break;
+            }
         }
 
         return shortNumber;
