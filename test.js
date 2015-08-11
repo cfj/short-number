c4fcbdb821c9e9a5bfcf686b4247ea071c7fb068
Jonathan Svärdén <jonathan.svarden@softhouse.se>
2015-08-11 16:20:01 +0200

    use tape for tests instead of ava

--- test.js
+++ test.js
@@ -1,13 +1,15 @@
 'use strict';
-var test = require('ava');
+var test = require('tape');
 var shortNumber = require('./');
 
-test(function(t) {
-    t.assert(shortNumber(231) === 231);
-    t.assert(shortNumber(5432) === '5.4K');
-    t.assert(shortNumber(35656) === '35.7K');
-    t.assert(shortNumber(1236903) === '1.2M');
-    t.assert(shortNumber(49653234) === '49.7M');
-    t.assert(shortNumber(984938293) === '984.9M');
-    t.assert(shortNumber(2743000000) === '2.7B');
+test('positive tests', function(t) {
+    t.plan(7);
+
+    t.equal(shortNumber(231), 231);
+    t.equal(shortNumber(5432), '5.4K');
+    t.equal(shortNumber(35656), '35.7K');
+    t.equal(shortNumber(1236903), '1.2M');
+    t.equal(shortNumber(49653234), '49.7M');
+    t.equal(shortNumber(984938293), '984.9M');
+    t.equal(shortNumber(2743000000), '2.7B');
 });
