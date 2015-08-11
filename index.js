5ba174b19966b8a4bd78a5ff95859d2b81433515
Jonathan Svärdén <jonathan.svarden@gmail.com>
2015-08-11 19:17:30 +0200

    add support for negative numbers

--- index.js
+++ index.js
@@ -14,19 +14,23 @@
             throw new RangeError('Input expected to be < 1e19');
         }
 
-        if(num < 1000) {
+        if(Math.abs(num) < 1000) {
             return num;
         }
 
         var shortNumber;
         var exponent;
+        var size;
+        var sign = num < 0 ? '-' : '';
         var suffixes = {
             'K': 6,
             'M': 9,
             'B': 12,
             'T': 16
         };
-        var size = Math.floor(Math.log(num) / Math.log(10)) + 1;
+
+        num = Math.abs(num);
+        size = Math.floor(Math.log(num) / Math.log(10)) + 1;
 
         exponent = size % 3 === 0 ? size - 3 : size - (size % 3);
         shortNumber = Math.round(10 * (num / Math.pow(10, exponent))) / 10;
@@ -38,6 +42,6 @@
             }
         }
 
-        return shortNumber;
+        return sign + shortNumber;
     };
 }));
--- test.js
+++ test.js
@@ -14,6 +14,18 @@ test('positive tests', function(t) {
     t.equal(shortNumber(2743000000), '2.7B');
 });
 
+test('negative tests', function(t) {
+    t.plan(7);
+
+    t.equal(shortNumber(-231), -231);
+    t.equal(shortNumber(-5432), '-5.4K');
+    t.equal(shortNumber(-35656), '-35.7K');
+    t.equal(shortNumber(-1236903), '-1.2M');
+    t.equal(shortNumber(-49653234), '-49.7M');
+    t.equal(shortNumber(-984938293), '-984.9M');
+    t.equal(shortNumber(-2743000000), '-2.7B');
+});
+
 test('throw error on too large numbers', function(t) {
     t.plan(1);
 
